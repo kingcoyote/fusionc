dfa9c05bca830fdda52ce632b80f39e753a33569
Steve Phillips <kingcoyote85@gmail.com>
2013-08-03 09:27:33 -0700

    moved GuiManager logic into a base FusionGameState class that all other game states will extend

---
+++ FusionC/FusionC/FusionGameState.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Nuclex.Game.States;
+using Nuclex.Input;
+using Nuclex.UserInterface;
+
+namespace FusionC
+{
+    abstract class FusionGameState : DrawableGameState
+    {
+        protected GuiManager Gui;
+        protected FusionGame Game;
+
+        public FusionGameState(FusionGame game)
+        {
+            Game = game;
+
+            Gui = new GuiManager(Game.Graphics, new InputManager())
+                {
+                    Screen = new Screen(game.GraphicsDevice.Viewport.Width, game.GraphicsDevice.Viewport.Height)
+                };
+
+            Gui.Screen.Desktop.Bounds = new UniRectangle(
+              new UniScalar(0.0f, 0.0f), new UniScalar(0.0f, 0.0f),
+              new UniScalar(0.8f, 0.0f), new UniScalar(0.8f, 0.0f)
+            );
+
+            Gui.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Gui.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Gui.Draw(gameTime);
+        }
+    }
+}
--- FusionC/FusionC/States/MainMenuState.cs
+++ FusionC/FusionC/States/MainMenuState.cs
@@ -3,56 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Nuclex.Game.States;
-using Nuclex.Input;
-using Nuclex.UserInterface;
-using Nuclex.UserInterface.Controls.Desktop;
-
 namespace FusionC.States
 {
-    class MainMenuState : DrawableGameState
+    class MainMenuState : FusionGameState
     {
-        private GuiManager _gui;
-        private FusionGame _game;
-
-        public MainMenuState(FusionGame game)
+        public MainMenuState(FusionGame game) : base(game)
         {
-            _game = game;
-
-            _gui = new GuiManager(_game.Graphics, new InputManager())
-                {
-                    Screen = new Screen(game.GraphicsDevice.Viewport.Width, game.GraphicsDevice.Viewport.Height)
-                };
-
-            _gui.Screen.Desktop.Bounds = new UniRectangle(
-              new UniScalar(0.0f, 0.0f), new UniScalar(0.0f, 0.0f),
-              new UniScalar(0.8f, 0.0f), new UniScalar(0.8f, 0.0f)
-            );
-
-            _gui.Screen.Desktop.Children.Add(new DemoDialog());
-
-            _gui.Initialize();
-        }
-
-        protected override void OnResume()
-        {
-            _game.IsMouseVisible = true;
-        }
-
-        protected override void OnPause()
-        {
-            _game.IsMouseVisible = false;
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            _gui.Update(gameTime);
-        }
-
-        public override void Draw(GameTime gameTime)
-        {
-            _gui.Draw(gameTime);
+            Gui.Screen.Desktop.Children.Add(new DemoDialog());
         }
     }
 }
