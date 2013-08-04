84362d72eb822a909714642a685bcdbc69f8035c
Steve Phillips <kingcoyote85@gmail.com>
2013-08-04 11:49:29 -0700

    added new gui skin for the main menu. it's not loading properly since it can't find the bitmap, but the rest seems to work.

--- FusionC/FusionC/States/MainMenuState.cs
+++ FusionC/FusionC/States/MainMenuState.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+
+using Nuclex.UserInterface.Visuals.Flat;
+
 namespace FusionC.States
 {
     class MainMenuState : FusionGameState
@@ -14,6 +17,9 @@ namespace FusionC.States
             mainMenuDialog.Bounds.Location.Y = game.GraphicsDevice.Viewport.TitleSafeArea.Height - 25 - mainMenuDialog.Bounds.Size.Y;
 
             Gui.Screen.Desktop.Children.Add(mainMenuDialog);
+
+            Gui.Visualizer = FlatGuiVisualizer.FromFile(game.Services, "Content/menu_gui.xml");
+
             Game.IsMouseVisible = true;
         }
     }
