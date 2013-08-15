b079d510c7a03f0cda67541f3c82458381748e1b
Steve Phillips <kingcoyote85@gmail.com>
2013-08-14 19:52:30 -0700

    added row of buttons for weapon hotkeys. these use the newImageButtonControl that i need to flesh out.

--- FusionC/FusionC/CombatWeaponDialog.Designer.cs
+++ FusionC/FusionC/CombatWeaponDialog.Designer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Nuclex.UserInterface;
+using Nuclex.UserInterface.Controls.Desktop;
 
 namespace FusionC
 {
@@ -10,7 +11,59 @@ namespace FusionC
     {
         private void InitializeComponent()
         {
-            Bounds = new UniRectangle(0, 0, 300, 80);
+            Bounds = new UniRectangle(0, 0, 275, 80);
+
+            // create row of hotbar icons for the 4 weapon types:
+            //   cannon
+            //   laser
+            //   missile
+            //   machinegun
+
+            ImageButtonControl hotkey;
+
+            // cannon
+            hotkey = new ImageButtonControl();
+            hotkey.Bounds = new UniRectangle(
+                new UniScalar(0.0F, 15),
+                new UniScalar(0.0F, 15),
+                new UniScalar(0.0F, 50), 
+                new UniScalar(0.0F, 50)
+            );
+            hotkey.Text = "CAN";
+            Children.Add(hotkey);
+
+            // laser
+            hotkey = new ImageButtonControl();
+            hotkey.Bounds = new UniRectangle(
+                new UniScalar(0.0F, 80),
+                new UniScalar(0.0F, 15),
+                new UniScalar(0.0F, 50),
+                new UniScalar(0.0F, 50)
+            );
+            hotkey.Text = "LAS";
+            Children.Add(hotkey);
+
+            // missile
+            hotkey = new ImageButtonControl();
+            hotkey.Bounds = new UniRectangle(
+                new UniScalar(0.0F, 145),
+                new UniScalar(0.0F, 15),
+                new UniScalar(0.0F, 50),
+                new UniScalar(0.0F, 50)
+            );
+            hotkey.Text = "MIS";
+            Children.Add(hotkey);
+
+            // machine gun
+            hotkey = new ImageButtonControl();
+            hotkey.Bounds = new UniRectangle(
+                new UniScalar(0.0F, 210),
+                new UniScalar(0.0F, 15),
+                new UniScalar(0.0F, 50),
+                new UniScalar(0.0F, 50)
+            );
+            hotkey.Text = "MG";
+            Children.Add(hotkey);
         }
     }
 }
---
+++ FusionC/FusionC/ImageButtonControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Nuclex.UserInterface.Controls;
+using Nuclex.UserInterface.Controls.Desktop;
+
+namespace FusionC
+{
+    public class ImageButtonControl : ButtonControl
+    {
+        public Texture2D Image;
+    }
+}
