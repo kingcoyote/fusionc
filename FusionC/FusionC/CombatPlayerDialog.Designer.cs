6c68a127a74e1264ece3229008877dbc99b916b9
Steve Phillips <kingcoyote85@gmail.com>
2013-08-21 20:14:57 -0700

    added a progress bar to show the player health

--- FusionC/FusionC/CombatPlayerDialog.Designer.cs
+++ FusionC/FusionC/CombatPlayerDialog.Designer.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Nuclex.UserInterface;
+using Nuclex.UserInterface.Controls;
 
 namespace FusionC
 {
     partial class CombatPlayerDialog
     {
+        private ProgressControl _healthbar;
+
         private void InitializeComponent()
         {
+            _healthbar = new ProgressControl();
+            _healthbar.Bounds = new UniRectangle(
+                new UniScalar(0.0F, 15),    
+                new UniScalar(0.0F, 15),
+                new UniScalar(270),
+                new UniScalar(35)
+            );
+            _healthbar.Progress = 0.5F;
+
+            Children.Add(_healthbar);
+
             Bounds = new UniRectangle(0, 0, 300, 80);
             EnableDragging = false;
         }
